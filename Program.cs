6fd331cba7883e799afe9be839143dd4bf26c994
tomrosso <trosso2024@chadwickschool.org>
2022-01-13 12:10:01 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trosso2024_BinaryNumbersinCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
